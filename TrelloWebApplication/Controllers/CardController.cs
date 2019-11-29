52c0daf31e7975866136de64ed9192450b886284
nunzio22 <nunzio22598@gmail.com>
2019-11-29 14:29:12 +0100

    prova

--- TrelloWebApplication/Controllers/CardController.cs
+++ TrelloWebApplication/Controllers/CardController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using TrelloWebApplication.Models;
 
 //api neccesarie : https://api.trello.com/1/card/[idcard]/checklists?key=9936fabac5fdc5f00e46ff3a454e9feb&token=27f3bbdeb9724521082f710e5dafbb9cfb56b315d90b2a27d502a6a391abad01
 namespace TrelloWebApplication.Controllers
@@ -11,9 +15,29 @@ namespace TrelloWebApplication.Controllers
     {
         public ActionResult Index()
         {
-            
 
-            return View(cards);
+            string prov = ("https://api.trello.com/b/5ddd5dad735c842669b7b819?key=9936fabac5fdc5f00e46ff3a454e9feb&token=27f3bbdeb9724521082f710e5dafbb9cfb56b315d90b2a27d502a6a391abad01");
+            WebRequest requestObj = WebRequest.Create(prov);
+            requestObj.Method = "GET";
+            HttpWebResponse responseObj = null;
+            responseObj = (HttpWebResponse)requestObj.GetResponse();
+            string result = null;
+
+            using (Stream stream = responseObj.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                result = sr.ReadToEnd();
+                sr.Close();
+            }
+
+            // store all data from the given file into a data variable
+            var data = File.ReadAllText(result);
+            
+            // deserialize data. After deserialization, our object json will be 
+            // populated with information from JSON file
+            var serializer = new JavaScriptSerializer();
+            var json = serializer.Deserialize<Card>(data);
+            return View();
         }
 
     }
