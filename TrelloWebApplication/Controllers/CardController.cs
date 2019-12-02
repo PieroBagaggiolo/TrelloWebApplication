a0b2cf4d552997b2d06514dcbfcbaea475c7f5b6
PieroBagaggiolo <56152505+PieroBagaggiolo@users.noreply.github.com>
2019-12-02 12:38:17 +0100

    ExportPDF in CardController

--- TrelloWebApplication/Controllers/CardController.cs
+++ TrelloWebApplication/Controllers/CardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rotativa;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,10 +15,34 @@ namespace TrelloWebApplication.Controllers
 {
     public class CardController : Controller
     {
+        List<Card> model = PopolateModel.Popola();
         public ActionResult Index()
         {
             var model = PopolateModel.Popola();
             return View(model);
+        }
+
+        public ActionResult Details(string id = null)
+        {
+            Card card=null;
+            foreach (var item in model)
+            {
+                if (item.Id==id)
+                {
+                    card = item;
+                }
+            }
+          
+            return View(card);
+        }
+
+        public ActionResult ExportPDF()
+        {
+            ActionAsPdf result = new ActionAsPdf("Index")
+            {
+                FileName = Server.MapPath("/Content/Details.pdf")
+            };
+            return result;
         }
 
     }
