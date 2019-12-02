49036523d2dc140812ca817ec22f38d083d486ec
PieroBagaggiolo <56152505+PieroBagaggiolo@users.noreply.github.com>
2019-12-02 12:03:59 +0100

    From master

---
+++ TrelloWebApplication/Controllers/CardController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using TrelloWebApplication.Models;
+using TrelloWebApplication.Utiliti;
+
+
+namespace TrelloWebApplication.Controllers
+{
+    public class CardController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = PopolateModel.Popola();
+            return View(model);
+        }
+
+    }
+}
---
+++ TrelloWebApplication/Utiliti/PopolateModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+using TrelloWebApplication.Models;
+
+namespace TrelloWebApplication.Utiliti
+{
+    public class PopolateModel
+    {
+        public static List<Card> Popola()
+        {
+            var cardtot = Api.Card();
+            var listC = Api.List();
+            foreach (var card in cardtot)
+            {
+                foreach (var list in listC)
+                {
+                    if (list.Id == card.IdList)
+                    {
+                        card.IdList = list.Name;
+                    }
+                }
+            }
+            foreach (var card in cardtot)
+            {
+                if (card.Badges.Attachments != "0")
+                {
+
+                    var allegato = Api.Img(card.Id);
+
+                    card.Attachments = allegato;
+                }
+                int m = Int32.Parse(card.Badges.CheckItems);
+                if (m > 0)
+                {
+                    var checklist = Api.Checked(card.Id);
+                    card.ChekedLists = checklist;
+
+                }
+            }
+         return cardtot;           
+        }
+    }
+}
