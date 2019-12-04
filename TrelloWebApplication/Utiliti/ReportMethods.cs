31107d84419f329ad53ad98f19be8fcc4388ae33
PieroBagaggiolo <56152505+PieroBagaggiolo@users.noreply.github.com>
2019-12-04 15:16:00 +0100

    Update ReportMethods.cs

--- TrelloWebApplication/Utiliti/ReportMethods.cs
+++ TrelloWebApplication/Utiliti/ReportMethods.cs
@@ -22,6 +22,19 @@ namespace TrelloWebApplication.Utiliti
             workSheet.Row(1).Height = 20;
             workSheet.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
             workSheet.Row(1).Style.Font.Bold = true;
+
+            workSheet.Cells[1, 1].Value = "ID";
+            workSheet.Cells[1, 2].Value = "STATO";
+            workSheet.Cells[1, 3].Value = "LABEL";
+            workSheet.Cells[1, 4].Value = "CHECKLIST";
+            workSheet.Cells[1, 1].Value = "ATTACHMENTS";
+            workSheet.Cells[1, 2].Value = "EXPIRE TIME";
+
+            //CORPO DELLA TABELLA
+
+
+
+
         }
     }
 }
