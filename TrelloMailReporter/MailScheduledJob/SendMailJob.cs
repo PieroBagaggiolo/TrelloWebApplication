14976e1a7febb8c535bafb408f9cecd79934a523
nunzio22 <nunzio22598@gmail.com>
2020-01-07 10:42:42 +0100

    delegate implements method

--- TrelloMailReporter/MailScheduledJob/SendMailJob.cs
+++ TrelloMailReporter/MailScheduledJob/SendMailJob.cs
@@ -14,15 +14,8 @@ namespace TrelloMailReporter.MailScheduledJob
 {
     public class SendMailJob : IJob
     {
-        // elementi neccessari per fare le chiamate in caso di neccessita di potrebbe fare una view che le chieda al utente
-        static string Key = "9936fabac5fdc5f00e46ff3a454e9feb";
-        static string Token = "27f3bbdeb9724521082f710e5dafbb9cfb56b315d90b2a27d502a6a391abad01";
-        static string IdBoard = "5ddd5dad735c842669b7b819";
-        // creazione del mio modello di api per le chiamate
-        static Api myApi = new Api(Key, Token, IdBoard);
-        //creazione del modello di liste di card
-        List<Card> model = PopolateModel.Popola(myApi);
 
+        public delegate void Del(ref ExcelPackage fileEx);
         public Task Execute(IJobExecutionContext context)
         {
             return Task.Factory.StartNew(() => SendMail());
@@ -33,14 +26,16 @@ namespace TrelloMailReporter.MailScheduledJob
         public void SendMail()
         {
             //creazione allegato excel prima di inviare la mail
-            ExcelPackage ex = ReportMethods.ExportExcelTotal(model);
+            Del del = ReportMethods.DelegateMethod;
+            ExcelPackage ex = new ExcelPackage();
+            del(ref ex);
             using (var memoryStream = new MemoryStream())
             {
                 ex.SaveAs(memoryStream);
                 memoryStream.Position = 0;
                 //Crea oggetto di tipo MailMessage
                 MailMessage Msg = new MailMessage();
-
+                
                 //Imposta il mittente
                 Msg.From = new MailAddress("trelloreporterapp@hotmail.com", "Limi");
 
