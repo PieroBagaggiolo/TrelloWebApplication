ebc946ec355a041cc5c9c101781fbe8807b3ff69
PieroBagaggiolo <56152505+PieroBagaggiolo@users.noreply.github.com>
2020-01-07 17:30:56 +0100

    Update EmailsController.cs

--- TrelloWebApplication/Controllers/EmailsController.cs
+++ TrelloWebApplication/Controllers/EmailsController.cs
@@ -23,6 +23,21 @@ namespace TrelloWebApplication.Controllers
             return View(db.Emails.ToList());
         }
 
+        // GET: Emails/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Email email = db.Emails.Find(id);
+            if (email == null)
+            {
+                return HttpNotFound();
+            }
+            return View(email);
+        }
+
         // GET: Emails/Create
         public ActionResult Create()
         {
@@ -30,7 +45,7 @@ namespace TrelloWebApplication.Controllers
         }
 
         // POST: Emails/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -43,49 +58,47 @@ namespace TrelloWebApplication.Controllers
                 email.Password = criptate;
                 db.Emails.Add(email);
                 db.SaveChanges();
-                string decrypt = SecurityPWD.Decrypt(criptate);
                 return RedirectToAction("Index");
             }
 
             return View(email);
         }
 
-        //// GET: Emails/Edit/5
-        //public ActionResult Edit(int id)
-        //{
-        //    if (id == 0)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Email email = db.Emails.Find(id);
-        //    if (email == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(email);
-        //}
+        // GET: Emails/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Email email = db.Emails.Find(id);
+            if (email == null)
+            {
+                return HttpNotFound();
+            }
+            return View(email);
+        }
 
-        //// POST: Emails/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "SenderEmail,Password,ReceiverEmail")] Email email)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(email).State = EntityState.Modified;
-
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(email);
-        //}
+        // POST: Emails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SenderEmail,Password,ReceiverEmail")] Email email)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(email).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(email);
+        }
 
         // GET: Emails/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(string id)
         {
-            if (id == 0)
+            if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
@@ -100,7 +113,7 @@ namespace TrelloWebApplication.Controllers
         // POST: Emails/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult DeleteConfirmed(string id)
         {
             Email email = db.Emails.Find(id);
             db.Emails.Remove(email);
