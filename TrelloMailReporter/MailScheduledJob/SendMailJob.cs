baf8fda1a849eb88b78402ae95abdae186f0de4e
nunzio22 <nunzio22598@gmail.com>
2020-01-08 15:10:02 +0100

    fix email send and remove file apimodel

--- TrelloMailReporter/MailScheduledJob/SendMailJob.cs
+++ TrelloMailReporter/MailScheduledJob/SendMailJob.cs
@@ -28,70 +28,73 @@ namespace TrelloMailReporter.MailScheduledJob
         /// </summary>
         public void SendMail()
         {
-            Email [] p=db.Emails.ToArray();
-            string emailSend = p[0].SenderEmail;
-            string password = p[0].Password;
-            string decrypt = SecurityPWD.Decrypt(password);
-            using (var memoryStream = new MemoryStream())
+            int i = db.Emails.Count();
+            if (2<=db.Emails.Count())
             {
-                //creazione allegato excel prima di inviare la mail
-                Del del = ReportMethods.DelegateMethod;
-                ExcelPackage ex = new ExcelPackage();
-                del(ref ex);
-                ex.SaveAs(memoryStream);
-                memoryStream.Position = 0;
-                //Crea oggetto di tipo MailMessage
-                MailMessage Msg = new MailMessage();
-                //Imposta il mittente
-                //Msg.From = new MailAddress("trelloreporterapp@hotmail.com", "Limi");
-                Msg.From = new MailAddress(emailSend, "Limi");
-                //La proprietà .To è una collezione di destinatari,
-                //quindi possiamo addizionare quanti destinatari vogliamo.
-                var primo = 0;
-                foreach (var mailCredentials in db.Emails.ToArray())
+                Email [] p=db.Emails.ToArray();
+                string emailSend = p[0].SenderEmail;
+                string password = p[0].Password;
+                string decrypt = SecurityPWD.Decrypt(password);
+                using (var memoryStream = new MemoryStream())
                 {
-                    if (primo==0)
+                    //creazione allegato excel prima di inviare la mail
+                    Del del = ReportMethods.DelegateMethod;
+                    ExcelPackage ex = new ExcelPackage();
+                    del(ref ex);
+                    ex.SaveAs(memoryStream);
+                    memoryStream.Position = 0;
+                    //Crea oggetto di tipo MailMessage
+                    MailMessage Msg = new MailMessage();
+                    //Imposta il mittente
+                    //Msg.From = new MailAddress("trelloreporterapp@hotmail.com", "Limi");
+                    Msg.From = new MailAddress(emailSend, "Limi");
+                    //La proprietà .To è una collezione di destinatari,
+                    //quindi possiamo addizionare quanti destinatari vogliamo.
+                    var primo = 0;
+                    foreach (var mailCredentials in db.Emails.ToArray())
                     {
-                        primo = 1;
+                        if (primo==0)
+                        {
+                            primo = 1;
+                        }
+                        else
+                        {
+                            Msg.To.Add(new MailAddress(mailCredentials.ReceiverEmail, mailCredentials.ReceiverEmail));
+                        }
                     }
-                    else
-                    {
-                        Msg.To.Add(new MailAddress(mailCredentials.ReceiverEmail, mailCredentials.ReceiverEmail));
-                    }
+                    //Imposto oggetto
+                    Msg.Subject = "Mail notifiche TrelloReporterApp ";
+
+                    //Imposto contenuto
+                    Msg.Body = "Mail automatica di notifiche giornaliera";
+                    Msg.IsBodyHtml = true;
+
+                    //Creo l'allegato e lo passo alla mail
+                    var attachment = new System.Net.Mail.Attachment(memoryStream, "Report.xlsx");
+                    attachment.ContentType = new ContentType("application/vnd.ms-excel");
+                    Msg.Attachments.Add(attachment);
+
+                    //Imposto il Server Smtp
+                    SmtpClient Smtp = new SmtpClient("smtp.live.com", 25);
+
+                    //Possiamo impostare differenti metodi di spedizione.
+                    //Imposta consegna diretta.
+                    Smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                    //Alcuni Server SMTP richiedono l'accesso autenticato
+                    Smtp.UseDefaultCredentials = false;
+                    NetworkCredential Credential = new NetworkCredential(emailSend, decrypt);
+                    Smtp.Credentials = Credential;
+
+                    //Certificato SSL
+                    Smtp.EnableSsl = true;
+
+                    //Spediamo la mail
+                    Smtp.Send(Msg);
+
                 }
-                //Imposto oggetto
-                Msg.Subject = "Mail notifiche TrelloReporterApp ";
-
-                //Imposto contenuto
-                Msg.Body = "Mail automatica di notifiche giornaliera";
-                Msg.IsBodyHtml = true;
-
-                //Creo l'allegato e lo passo alla mail
-                var attachment = new System.Net.Mail.Attachment(memoryStream, "Report.xlsx");
-                attachment.ContentType = new ContentType("application/vnd.ms-excel");
-                Msg.Attachments.Add(attachment);
-
-                //Imposto il Server Smtp
-                SmtpClient Smtp = new SmtpClient("smtp.live.com", 25);
-
-                //Possiamo impostare differenti metodi di spedizione.
-                //Imposta consegna diretta.
-                Smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-
-                //Alcuni Server SMTP richiedono l'accesso autenticato
-                Smtp.UseDefaultCredentials = false;
-                NetworkCredential Credential = new NetworkCredential(emailSend, decrypt);
-                Smtp.Credentials = Credential;
-
-                //Certificato SSL
-                Smtp.EnableSsl = true;
-
-                //Spediamo la mail
-                Smtp.Send(Msg);
 
             }
-
-
         }
     }
 }
